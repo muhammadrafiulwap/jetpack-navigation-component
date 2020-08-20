dafa177ad89ce55fdcdec45e5c9b7ff9458d7b64
Programmer <programmer@localhost.localdomain>
2020-08-20 13:25:35 +0800

    [Add] implement navigation and passing data

---
+++ app/src/main/java/com/udacoding/jetpacknavigationcomponent/SpecifyAmountFragment.kt
@@ -0,0 +1,49 @@
+package com.udacoding.jetpacknavigationcomponent
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_specify_amount.*
+
+class SpecifyAmountFragment : Fragment(), View.OnClickListener {
+
+    lateinit var navController: NavController
+    lateinit var get_recipient: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        get_recipient = arguments?.getString("recipient").toString()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_specify_amount, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        send_btn.setOnClickListener(this)
+        cancel_btn.setOnClickListener(this)
+
+        val message = "Sending money to $get_recipient"
+        recipient.text = message
+    }
+
+    override fun onClick(view: View?) {
+
+        when(view?.id){
+            R.id.send_btn -> navController.navigate(R.id.action_specifyAmountFragment_to_confirmationFragment)
+            R.id.cancel_btn -> activity?.onBackPressed()
+        }
+
+    }
+
+}
